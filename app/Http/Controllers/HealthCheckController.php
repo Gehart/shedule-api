52780a3291940a96189a48835c3693387bd9c8fe
g.orlov <g.orlovi@ya.ru>
2022-04-17 23:23:58 +0300

    Doctrine setup

--- app/Http/Controllers/HealthCheckController.php
+++ app/Http/Controllers/HealthCheckController.php
@@ -2,42 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use App\Entities\Post;
 use Doctrine\ORM\EntityManagerInterface;
 use Illuminate\Http\Request;
 
 class HealthCheckController extends Controller
 {
     /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
-
-    /**
      * @param Request $request
      * @param EntityManagerInterface $entityManager
+     * @return string
      */
-    public function check(Request $request, EntityManagerInterface $entityManager)
+    public function check(Request $request, EntityManagerInterface $entityManager): string
     {
-        $a = 4;
-        $b = $a;
-//        var_dump($b);
-//        $connection = pg_connect("host=postgres port=5432 dbname=postgres user=user password=password");
+        // проверяем соединение с БД
+        $entityManager->getConnection()->connect();
 
-        $post = new Post('test', 'it is a test');
-        $entityManager->persist($post);
-        $entityManager->flush();
-
-
-//        if ($connection) {
-//            return 'connected to postgresql';
-//        } else {
-//            return 'there has been an error connecting';
-//        }
+        return 'It\'s good';
     }
 }
